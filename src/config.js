6dd52a8a59244279e9d87ad3013cc556b16a5d91
Malek Haj-Hussein <malekadair@gmail.com>
2020-02-29 11:42:39 -0600

    updated DB_URL to DATABASE_URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://Malek@localhost/tournease",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://Malek@localhost/tournease",
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret"
 };
